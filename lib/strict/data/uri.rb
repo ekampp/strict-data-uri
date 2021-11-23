f6417a59a608f73c19a5e570b7ef764e7c574489
Emil Kampp <emil@kampp.me>
2021-11-23 19:37:43 +0100

    Remove unused error

--- lib/strict/data/uri.rb
+++ lib/strict/data/uri.rb
@@ -7,8 +7,6 @@ require "base64"
 module Strict
   module Data
     module Uri
-      class Error < StandardError; end
-
       def self.encode(data, type = "text/plain")
         "data:#{type};base64,#{Base64.strict_encode64(data).rstrip}"
       end
